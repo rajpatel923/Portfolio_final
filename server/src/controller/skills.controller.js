36b058a0552423dc1e9d40e1bff582446ec00f6f
rajpatel923 <rajpatel123370@gmail.com>
2024-04-14 20:32:24 -0500

    add skill check

--- server/src/controller/skills.controller.js
+++ server/src/controller/skills.controller.js
@@ -3,8 +3,13 @@ import {ApiError} from "../utils/ApiError.js"
 import {Skills} from "../models/skills.model.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 
-const addSkill = asyncHandler(async(req,res,next)=>{
-    console.log("you are in add skill function");
+const addSkill = asyncHandler(async(req,res)=>{
+   const {skillName} = req.body
+
+   if(skillName ===""){
+        throw new ApiError(400, "For adding skill a skill is must!")
+    }
+    const skillExist = await Skills.findOne({skillName})
 })
 
 export {addSkill}
