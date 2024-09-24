2c3ffe0e0b2540774ef25e1e6b76a84b92e9002e
rajpatel923 <rajpatel123370@gmail.com>
2024-09-23 23:54:42 -0500

    back to last version

--- Client/src/layouts/RootLayout.tsx
+++ Client/src/layouts/RootLayout.tsx
@@ -37,7 +37,7 @@ const RootLayout = ({heroSectionHeadingOne, heroSectionHeadingTwo, heroSectionHe
   heroSectionHeadingThree === "" ? "Full stack Developer & Machine Learning Engineer": heroSectionHeadingThree;
   return (
       <div className=' flex lg:items-center justify-center w-full h-screen z-2 '>
-        <div className=' lg:flex lg:flex-col w-full lg:h-[700px]  lg:border-[14px] border-black border-solid shadow-sm rounded-xl text-white lg:max-w-[1140px] '>
+        <div className=' flex flex-col w-full lg:h-[700px]  lg:border-[14px] border-black border-solid shadow-sm rounded-xl text-white lg:max-w-[1140px] '>
           <header className='z-10 w-full lg:static '>
             <NavBar/>
             <div className='w-full border-t-[0.75px] border-gray-500/75'></div>
