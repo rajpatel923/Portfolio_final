c5c9657793f519482817617ce5c3854da2ea0823
rajpatel923 <rajpatel123370@gmail.com>
2026-03-13 17:02:43 -0500

    refactor sendMail controller to use a single transporter function and simplify email sending logic

--- server/src/controller/sendMail.controller.js
+++ server/src/controller/sendMail.controller.js
@@ -7,7 +7,9 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 
 const createTransporter = () =>
   nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.NODEMAILER_USER,
       pass: process.env.NODEMAILER_PASS,
