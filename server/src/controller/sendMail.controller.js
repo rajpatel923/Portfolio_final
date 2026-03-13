a44654313506b39ebe11943e1d3af7842c04c267
rajpatel923 <rajpatel123370@gmail.com>
2026-03-13 16:50:41 -0500

    refactor sendMail controller to use a single transporter function and simplify email sending logic

--- server/src/controller/sendMail.controller.js
+++ server/src/controller/sendMail.controller.js
@@ -4,43 +4,29 @@ import handlebars from "handlebars";
 import fs from "fs";
 import path from "path";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { ApiError } from "../utils/ApiError.js";
+
+const createTransporter = () =>
+  nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.NODEMAILER_USER,
+      pass: process.env.NODEMAILER_PASS,
+    },
+  });
 
 const notifyProvider = async (fullName, email, phoneNumber, message) => {
   const filePath = path.join(process.cwd(), "public", "html", "sender.html");
   const source = fs.readFileSync(filePath, "utf-8").toString();
   const template = handlebars.compile(source);
-  const replacementsOfSender = {
-    fullName: fullName,
-    phoneNumber: phoneNumber,
-    email,
-    email,
-    message: message,
-  };
+  const htmlToSend = template({ fullName, phoneNumber, email, message });
 
-  const htmlToSend = template(replacementsOfSender);
-  let mailTransporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "rajpatel123370@gmail.com",
-      pass: process.env.NODEMAILER_PASS,
-    },
-  });
-  let receiver = {
-    from: "rajpatel123370@gmail.com",
+  await createTransporter().sendMail({
+    from: process.env.NODEMAILER_USER,
     to: "patelraj2@yahoo.com",
     subject: "New Contact Request",
     text: "Got new connection !",
     html: htmlToSend,
     headers: { "x-myheader": "test header" },
-  };
-
-  mailTransporter.sendMail(receiver, function (err, data) {
-    if (err) {
-      console.log(err);
-    } else {
-      throw new ApiError(401, "Unable to send mail to provider");
-    }
   });
 };
 
@@ -52,37 +38,18 @@ const sendMail = asyncHandler(async (req, res) => {
   const filePath = path.join(process.cwd(), "public", "html", "receiver.html");
   const source = fs.readFileSync(filePath, "utf-8").toString();
   const template = handlebars.compile(source);
-  const replacements = {
-    firstName: fullName,
-  };
+  const htmlToSend = template({ firstName: fullName });
 
-  const htmlToSend = template(replacements);
-  let mailTransporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "rajpatel123370@gmail.com",
-      pass: process.env.NODEMAILER_PASS,
-    },
-  });
-  let receiver = {
-    from: "rajpatel123370@gmail.com",
+  await createTransporter().sendMail({
+    from: process.env.NODEMAILER_USER,
     to: email,
     subject: "Thank You for Contacting Me",
     text: "Don't worry I got your information !",
     html: htmlToSend,
     headers: { "x-myheader": "test header" },
-  };
-
-  mailTransporter.sendMail(receiver, function (err, data) {
-    if (err) {
-      console.log(err);
-      res
-        .status(200)
-        .json(new ApiResponse(200, data, "Successfully send the mail"));
-    } else {
-      console.log(data);
-    }
   });
+
+  res.status(200).json(new ApiResponse(200, {}, "Successfully sent the mail"));
 });
 
 export { sendMail };
